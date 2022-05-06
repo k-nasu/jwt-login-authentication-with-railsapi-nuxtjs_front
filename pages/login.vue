90e30b69d94d0689433f69f8c40870c80b8c4a13
kenta.nasu <k.nasu1996@gmail.com>
2022-05-06 19:20:04 +0900

    add user account settings page & password page

--- pages/login.vue
+++ pages/login.vue
@@ -50,19 +50,18 @@ export default {
     UserFormPassword
   },
   layout: 'before-login',
-  data () {
+  data ({ $store }) {
     return {
       isValid: false,
       loading: false,
-      params: { user: { email: '', password: '' } }
+      params: { user: { email: '', password: '' } },
+      redirectPath: $store.state.loggedIn.redirectPath
     }
   },
   methods: {
     login () {
       this.loading = true
-      setTimeout(() => {
-        this.loading = false
-      }, 1500)
+      this.$router.push(this.redirectPath)
     }
   }
 }
